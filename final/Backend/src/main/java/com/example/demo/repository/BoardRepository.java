b9806a91ad305e5678611d67dc5f306f0f80f813
rabent <rabent0207@gmail.com>
2025-05-10 23:53:46 +0900

    <feat>(repository) : 엔티티, 리포지토리 수정 및 파일업로드 추가
    board 엔티티에 작성 시간 추가, erd 수정 및 게시판 목록에서 content도 불러오는 문제 해결 위해 dto 생성, jpql 쿼리로 메서드 구현

--- final/Backend/src/main/java/com/example/demo/repository/BoardRepository.java
+++ final/Backend/src/main/java/com/example/demo/repository/BoardRepository.java
@@ -2,11 +2,14 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.model.dto.BoardListDto;
 import com.example.demo.model.entity.Board;
 import com.example.demo.model.entity.User;
 
@@ -14,16 +17,25 @@ import com.example.demo.model.entity.User;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-    // 특정 사용자가 작성한 모든 게시글 찾기
-    List<Board> findAllByUser(User user);
-    
-    // 제목에 특정 키워드가 포함된 게시글 찾기
-    List<Board> findByTitleContaining(String keyword);
-    
-    // 조회수 기준으로 내림차순 정렬하여 상위 N개 게시글 찾기
+    //게시판 리스트용 dto 페이징(상세는 findbyid로)
+    @Query("SELECT new com.example.demo.model.dto.BoardListDto(b.id, b.title, b.hit, b.user, b.createdAt) FROM Board b")
+    Page<BoardListDto> findAllForList(Pageable pageable);
+
+    // 제목 검색 결과를 페이징하여 가져오기(10개씩으로 구현 필요)
+    @Query("SELECT new com.example.demo.model.dto.BoardListDto(b.id, b.title, b.hit, b.user, b.createdAt) FROM Board b " + 
+    "WHERE b.title LIKE %:keyword% ")
+    Page<BoardListDto> findByTitleContaining(@Param("keyword") String keyword, Pageable pageable);
+
+    // 특정 사용자의 게시글을 페이징하여 가져오기
+    @Query("SELECT new com.example.demo.model.dto.BoardListDto(b.id, b.title, b.hit, b.user, b.createdAt) FROM Board b " + 
+    "WHERE b.user=:user ")
+    Page<BoardListDto> findByUser(@Param("user") User user, Pageable pageable);  
+    // 조회수 기준으로 내림차순 정렬하여 상위 N개 게시글 찾기(옵션)
     @Query("SELECT b FROM Board b ORDER BY b.hit DESC")
     List<Board> findTopNByOrderByHitDesc(Pageable pageable);
     
     // 특정 사용자가 작성한 게시글 수 카운트
     Long countByUser(User user);
+
+    
 }
