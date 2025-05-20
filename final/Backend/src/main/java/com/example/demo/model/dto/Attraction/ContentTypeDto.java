aa54043a0d36c65a48bdd181ab8b46ccfa34aa25
ks0927 <brian_als@naver.com>
2025-05-20 16:44:42 +0900

    feat: 시도, 컨텐츠타입, 군구, attraction 관련 api 추가

---
+++ final/Backend/src/main/java/com/example/demo/model/dto/Attraction/ContentTypeDto.java
@@ -0,0 +1,11 @@
+package com.example.demo.model.dto.Attraction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ContentTypeDto {
+    private int contentTypeId;
+    private String contentTypeName;
+}
