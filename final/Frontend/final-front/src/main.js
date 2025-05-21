17b56cca824733e24cac03112122f3adbbc7b11e
ks0927 <brian_als@naver.com>
2025-05-21 14:13:57 +0900

    feat: vue3-kakao-map 라이브러리 추가

--- final/Frontend/final-front/src/main.js
+++ final/Frontend/final-front/src/main.js
@@ -2,7 +2,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { useKakao } from 'vue3-kakao-maps/@utils';
 
+useKakao('27490439fa9e5e360bdbce0f425e5963')
 // Vue 3에서는 createApp() 함수 사용
 createApp(App)
   .use(router)
