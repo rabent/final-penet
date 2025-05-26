83616f50d04e95c1364d67d1d630046d51988e77
rabent <rabent0207@gmail.com>
2025-05-26 21:24:20 +0900

    <feat> : 기존 localhost:8080으로 고정되어있던 주소들 상대주소로 변경

--- final/Frontend/final-front/src/utils/axios.js
+++ final/Frontend/final-front/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import router from '@/router'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: '/api'
 })
 
 // 요청 인터셉터
@@ -27,12 +27,12 @@ api.interceptors.response.use(
       // 토큰 만료 코드 확인 (서버에서 보내는 특정 코드)
       const errorData = error.response.data
       const isTokenExpired = errorData && errorData.code === 'TOKEN_EXPIRED'
-      
+
       // 토큰이 만료되었거나 유효하지 않은 경우
       sessionStorage.removeItem('token')
       sessionStorage.removeItem('userId')
       router.push('/login')
-      
+
       if (isTokenExpired) {
         alert('로그인 세션이 만료되었습니다. 다시 로그인해주세요.')
       } else {
